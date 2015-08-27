8a230fa29c9193e6b2b0b1eb2a24f71eaa050b7f
nrock <nmacellaio@gmail.com>
2015-08-27 09:40:14 -0500

    added web project

---
+++ Calendar.Web/Calendar.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Calendar.Web.MvcApplication" Language="C#" %>
---
+++ Calendar.Web/Calendar.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Calendar.Web.Startup))]
+namespace Calendar.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
